fbf6bf659c3e6c5abe54c3426fb7c6ccdbfedb5e
František Žiačik <ziacik@gmail.com>
2026-09-18 21:09:27 +0200

    Add core/build.gradle.kts

---
+++ core/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+	id("com.android.library")
+}
+
+android {
+	namespace = "com.ziacik.cookcue.core"
+	compileSdk = 37
+
+	defaultConfig {
+		minSdk = 26
+	}
+
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_17
+		targetCompatibility = JavaVersion.VERSION_17
+	}
+}
+
+dependencies {
+	testImplementation("junit:junit:4.13.2")
+}
