a6956eeb602028f1335bf7165c1ec2aa190242a4
František Žiačik <ziacik@gmail.com>
2026-09-18 21:08:06 +0200

    Add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+	id("com.android.application") version "9.4.0" apply false
+	id("com.android.library") version "9.4.0" apply false
+	id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
+}
